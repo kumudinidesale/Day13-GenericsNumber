34100424ad1f93223be84311e2d3b6c8052a5a6d
kumudinidesale <kdesale1211@gmail.com>
2022-02-25 20:15:54 +0530

    added:Maximum Of Three Float

--- src/com/java/Maximum.java
+++ src/com/java/Maximum.java
@@ -1,8 +1,8 @@
 package com.java;
 
 public class Maximum {
-	public static Integer maxInteger(Integer a, Integer b, Integer c) {
-		Integer max = a;// assume a is initially largest
+	public static Float maxFloat(Float a, Float b, Float c) {
+		Float max = a;// assume a is initially largest
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -18,7 +18,7 @@ public class Maximum {
 	// main method
 	public static void main(String[] args) {
 		// Calling method with Integer argument
-		Maximum.maxInteger(5, 6, 7);
+		Maximum.maxFloat(5.7f, 6.8f, 7.9f);
 	}
 
 }
