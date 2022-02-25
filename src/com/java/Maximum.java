801c9b8b5149ce3509a832555f096f1a30e76eac
kumudinidesale <kdesale1211@gmail.com>
2022-02-25 20:24:15 +0530

    added:Maximum Of Three String

--- src/com/java/Maximum.java
+++ src/com/java/Maximum.java
@@ -1,8 +1,8 @@
 package com.java;
 
 public class Maximum {
-	public static Float maxFloat(Float a, Float b, Float c) {
-		Float max = a;// assume a is initially largest
+	public static String maxString(String a, String b, String c) {
+		String max = a;// assume a is initially largest
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
@@ -18,7 +18,7 @@ public class Maximum {
 	// main method
 	public static void main(String[] args) {
 		// Calling method with Integer argument
-		Maximum.maxFloat(5.7f, 6.8f, 7.9f);
+		Maximum.maxString("Mango", "orange", "Banana");
 	}
 
 }
